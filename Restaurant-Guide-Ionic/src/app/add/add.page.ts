5e8d57f0a652738c20e298397252551951292a9c
Dubalino <71103318+Dubalino@users.noreply.github.com>
2021-04-07 22:12:13 -0400

    edited the add pgae ts file

--- Restaurant-Guide-Ionic/src/app/add/add.page.ts
+++ Restaurant-Guide-Ionic/src/app/add/add.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.page.scss'],
 })
 export class AddPage implements OnInit {
+  name: string;
+  address: string;
+  phone: string;
+  description: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public toastController: ToastController) {
+    this.name = '';
+    this.address = '';
+    this.phone = '';
+    this.description = '';
   }
 
+  ngOnInit() {}
+
+  async addRestaurant() {
+    console.log({
+      name: this.name,
+      address: this.address,
+      phone: this.phone,
+      description: this.description,
+    });
+
+    const toast = await this.toastController.create({
+      message: `${this.name} added to the Database`,
+      duration: 2000,
+      position: 'bottom',
+      color: "success"
+    });
+
+    toast.present();
+  }
 }
