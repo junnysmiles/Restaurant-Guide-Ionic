87a90fc4334d3bd16ade0b230b265d0b948ca458
Stefan Maric <stefanmaric.developer@gmail.com>
2021-04-08 00:22:35 -0400

    Adding student info, fix.

--- Restaurant-Guide-Ionic/src/app/about/about.page.ts
+++ Restaurant-Guide-Ionic/src/app/about/about.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Developer {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+developers: Developer[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.developers = [
+      { id: 101197834, name: 'Jun yan Gan' },
+      { id: 101208175, name: 'Stefan Maric' },
+      { id: 101210892, name: 'Kevin Silva' },
+    ];
   }
 
 }
