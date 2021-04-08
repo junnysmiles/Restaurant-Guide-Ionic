60bb84f639b4700e17f97b365b1b0a343d40876f
Dubalino <71103318+Dubalino@users.noreply.github.com>
2021-04-07 23:08:13 -0400

    edited the favourites ts file

--- Restaurant-Guide-Ionic/src/app/favourites/favourites.page.ts
+++ Restaurant-Guide-Ionic/src/app/favourites/favourites.page.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+interface Restaurant {
+  id: number;
+  name: string;
+  likes: number;
+  address: string;
+  description: string;
+}
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
 })
 export class FavouritesPage implements OnInit {
+  restaurants: Restaurant[];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.restaurants = [
+      {
+        id: 1,
+        name: 'Mongolian Hot Pot',
+        likes: 3,
+        address: 'address123',
+        description: 'I love this pace, dont forget about ...',
+      },
+
+      {
+        id: 2,
+        name: 'Korean BBQ',
+        likes: 5,
+        address: 'address123',
+        description: 'The Waguy beef is to die for...',
+      },
+
+      {
+        id: 3,
+        name: 'Sushi Teppanyaki',
+        likes: 4,
+        address: 'address123',
+        description: 'Expensive but worth it, servers...',
+      },
+    ];
   }
 
+  debug(r: any) {
+    console.log(r);
+  }
+
+  counter(i: number) {
+    return new Array(i);
+  }
+
+  remove(id: number) {
+    this.restaurants = this.restaurants.filter((r) => r.id !== id);
+  }
+
+  navigate(r: Restaurant) {
+    this.router.navigate(["/restaurant", r],)
+  }
 }
