b35ba53920591862537e8205ac453ff7aab56573
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2021-04-14 22:56:36 -0400

    created drawer navigation

--- Restaurant-Guide-Ionic/src/app/app.component.ts
+++ Restaurant-Guide-Ionic/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router, public toastController: ToastController) {}
+
+  ngOnInit() {}
+
+  async logout() {
+    this.router.navigate(['/']);
+
+    let toast = await this.toastController.create({
+      message: 'Logged Out',
+      duration: 2000,
+      position: 'bottom',
+      color: 'success',
+    });
+
+    toast.present();
+  }
 }
--- Restaurant-Guide-Ionic/src/app/components/navbar/navbar.component.ts
+++ Restaurant-Guide-Ionic/src/app/components/navbar/navbar.component.ts
@@ -1,31 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
   @Input('page') page: any;
 
-  constructor(private router: Router, public toastController: ToastController) {}
-
-  ngOnInit() {
-    console.log('PAGE: ', this.page);
-  }
-
-  async logout() {
-    this.router.navigate(['/']);
-
-    let toast = await this.toastController.create({
-      message: 'Logged Out',
-      duration: 2000,
-      position: 'bottom',
-      color: 'success',
-    });
-
-    toast.present();
-  }
+  constructor() {}
 }
